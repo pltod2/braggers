9a1836113e43bd6c888a50c4f5884e3aed6e53f2
vladimir-trifonov <trifonov.dev@gmail.com>
2016-11-08 11:02:06 +0200

    Added Slack-bot sample and Client-receiver microservices

---
+++ slack-bot-sample/readme.md
@@ -0,0 +1,12 @@
+## Usage:
+
+#### Slack bot
+##### `> cd slack-bot`
+##### `> node .`
+
+#### Slack's user info receiver
+##### `> cd user-receiver`
+##### `> SLACK_TOKEN={token} node .`
+
+#### In slack:
+##### `> /msg @{botName} export @{username}`
---
+++ slack-bot-sample/slack-bot/utils/slack.js
@@ -0,0 +1,28 @@
+var slackTerminalize = require('slack-terminalize');
+var webClient = slackTerminalize.getWebClient();
+
+// Get slack's user information'
+var getUserInfo = function (username, cb) {
+	webClient.users.info(username, function (err, response) {
+		if (err || (response && !response.ok)) {
+			return cb(err ? err : response);
+		}
+
+		cb(response.user);
+	});
+};
+
+// Post message in slack's channel'
+var postMessage = function (channel, response) {
+	response = '```' + response + '```';
+
+	webClient.chat.postMessage(channel, response, {
+		as_user: true
+	});
+};
+
+// Export the methods
+module.exports = {
+	postMessage: postMessage,
+	getUserInfo: getUserInfo
+};
