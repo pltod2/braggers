81e03b9a02d2757ff2a8954dd02790b54168bdea
Plamen Todorov <ptodorov@axway.com>
2016-11-22 14:45:37 +0200

    home page

--- app/ui/braggers.js
+++ app/ui/braggers.js
@@ -2,8 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Editor from './editor';
 
-export default class App extends React.Component {
+import styles from './braggers.less';
 
+//HERE IT IS HOW WE CREATE STYLES PROGRAMMATICALLY
+import compStyles from './braggers.css';
+const classStyles = compStyles.locals; 
+//TODO make factories for classes and styles
+const myDivClass = "box".concat(" ").concat(classStyles.headline);
+const startFreeFormField = classStyles.startFreeFormField;
+const startFreeButton = classStyles.startFreeBtn;
+const mySublineStyle = {
+    color: "rgb(254, 242, 221)"
+}
+
+export default class App extends React.Component {
 
     constructor(props) {
         super(props);
@@ -19,13 +31,24 @@ export default class App extends React.Component {
     }
 
     render() {
-        return <div className="row">
-                    <div className="col-xs-12 col-sm-8 col-md-6col-lg-4">
-                    dshgfkjdsjkfhs
-                        <div className="box">{this.state.message}</div>
-                        <div className="box"><button onClick={this.changeLabel}>Change Message</button></div>                    
-                        <div className="box"><Editor.MyEditor/></div>
-                    </div>
+        return <div>
+            <div className="row center-xs">
+                <div className="col-xs-6">
+                    <div className={myDivClass}>B R A G G E R S</div>
                 </div>
+            </div>
+            <div className="row center-xs">
+                <div className="col-xs-6">
+                    <h1 className="hdg hdg_1 mix-hdg_light" style={mySublineStyle}>share your ideas. make an impact.</h1>
+                </div>
+            </div>
+            <div className="row center-xs">
+                <div className="col-xs-3">
+                    <input className={startFreeFormField} name="email" type="text" placeholder="start for free with email" />
+                    <button className={startFreeButton} name="startFreeBTN" type="submit">Go</button>
+                </div>
+            </div>
+        </div> 
+        
     }
 }
