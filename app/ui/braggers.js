23250f814325b0e557b960aa1cca390519baae71
Plamen Todorov <ptodorov@axway.com>
2016-11-23 16:24:18 +0200

    adding use and administrate pages

--- app/ui/braggers.js
+++ app/ui/braggers.js
@@ -1,54 +1,33 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as Editor from './editor';
+import * as debug from "debug";
+const log = debug("BRAGGERS");
 
 import styles from './braggers.less';
+import Welcome from "./component/welcome/welcome";
+import Home from "./component/home/home";
 
-//HERE IT IS HOW WE CREATE STYLES PROGRAMMATICALLY
-import compStyles from './braggers.css';
-const classStyles = compStyles.locals; 
-//TODO make factories for classes and styles
-const myDivClass = "box".concat(" ").concat(classStyles.headline);
-const startFreeFormField = classStyles.startFreeFormField;
-const startFreeButton = classStyles.startFreeBtn;
-const mySublineStyle = {
-    color: "rgb(254, 242, 221)"
-}
+export default class Braggers extends React.Component {
 
-export default class App extends React.Component {
-
+    /**
+     * Internal APIs used as part of the application architecture 
+     */
     constructor(props) {
         super(props);
         this.state = {
-            message: "THIS IS MY REACT APP"
+            currentSection: "home",
+            authenticated: false
         }
     }
 
-    changeLabel = (selectedSection) => {
+    login = (selectedSection) => {
         this.setState({
-            message: "...AND I ENJOY IT A LOT!"
+            authenticated: true
         })
     }
 
     render() {
-        return <div>
-            <div className="row center-xs">
-                <div className="col-xs-6">
-                    <div className={myDivClass}>B R A G G E R S</div>
-                </div>
-            </div>
-            <div className="row center-xs">
-                <div className="col-xs-6">
-                    <h1 className="hdg hdg_1 mix-hdg_light" style={mySublineStyle}>share your ideas. make an impact.</h1>
-                </div>
-            </div>
-            <div className="row center-xs">
-                <div className="col-xs-3">
-                    <input className={startFreeFormField} name="email" type="text" placeholder="start for free with email" />
-                    <button className={startFreeButton} name="startFreeBTN" type="submit">Go</button>
-                </div>
-            </div>
-        </div> 
-        
+        const area = this.state.authenticated ? <Home /> : <Welcome loginHandler={this.login} />;
+        return <div>{area}</div>;
     }
 }
---
+++ app/ui/component/administrate/administrate.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import compStyles from './administrate.css';
+
+export default class Administration extends React.Component<{}, {}> {
+
+
+    render() {
+        return <div>
+            Administration TODO
+        </div>
+    }
+}
+
