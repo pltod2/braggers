bcc3cc762b13c757a2f7ccc0c8639eb0ee47b2ee
ngeorgieva <nat.m.georgieva@gmail.com>
2016-11-24 17:15:18 +0200

    a few fixes on github integration and users data collection logic

--- app/integration/github/index.js
+++ app/integration/github/index.js
@@ -1,40 +1,65 @@
 var github = require('octonode');
+var tokens = require(('./tokens.js'));
+var client = github.client({
+    username: tokens.githubUsername,
+    password: tokens.githubPassword
+});
+var ghsearch = client.search();
 
-module.exports = function(userEmail){
-
-    if(userEmail !== undefined){
-
-        var client = github.client();
-        var ghsearch = client.search();
-        var usersData = undefined;
+module.exports = {
+    getUserByEmail: function (userEmail, callback) {
 
         (function getUserInfo(userEmail, requestService) {
             ghsearch.users({
-            q: userEmail + ' in:email',
-            sort: 'created',
-            order: 'asc'
+                q: userEmail + ' in:email',
+                sort: 'created',
+                order: 'asc'
             }, function (err, data, headers) {
 
-            if (err) return console.log('Error:', err);
+                if (err) return console.log('Error:', err);
 
-            requestService(data);
+                requestService(data);
             })
         })(userEmail, requestService);
 
         function requestService(data) {
-         var username = data.items[0].login;
-        //console.log(username);
-       
-        if (username != null && username != undefined) {
-           client.get('/users/' + username, {}, 
-               function (err, status, body, headers) {
-                   printService(body)
-               });
-        }
-        }
+            //console.log(data);
+            if (data.total_count !== 0) {
+                var username = data.items[0].login;
+                //console.log(username);
 
-        function printService(body) {
-            console.log(body);
+                if (username !== null && username !== undefined) {
+                    client.get('/users/' + username, {},
+                        function (err, status, body, headers) {
+                            if (err) {
+                                return console.log("Error:" + err);
+                            }
+                            callback(body);
+                        });
+                }
+            } else {
+                callback(undefined);
+            }
         }
+    },
+
+    getUserRepos: function (username, callback) {
+        ghuser = client.user(username);
+        ghuser.repos(function (err, data, headers) {
+            if(err){
+                return consle.log("Error: " + err);
+            }
+            callback(data);
+        });
+    },
+
+    getUserFollowers: function (username, callback) {
+        ghuser = client.user(username);
+        ghuser.followers(function (err, data, headers) {
+            if(err){
+                return consle.log("Error: " + err);
+            }
+            callback(data);
+        });
     }
 }
