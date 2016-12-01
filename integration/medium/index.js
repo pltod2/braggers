6f0e3816610cbd79062a7f60a948ff210f22e3a1
ngeorgieva <nat.m.georgieva@gmail.com>
2016-12-01 09:54:13 +0200

    functionality for getting user post urls from medium added

---
+++ integration/medium/index.js
@@ -0,0 +1,5 @@
+var getUserPosts = require('./get-user-posts');
+//var mdParser = require('./url-to-md');
+
+//getUserPosts('patti.mulligan', mdParser.parseMediumPostsToMd, writer.writePosts);
+getUserPosts('patti.mulligan', console.log);
